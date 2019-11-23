66a5744ba90b31ea6709ddd471fa86287f2a0087
Richard Tourdot <rwtourdot@Richards-iMac.local>
2019-11-23 15:48:03 -0500

    added loop chrom files

--- loop_scripts/loop_chrom_scaffold_RPE1.sh
+++ loop_scripts/loop_chrom_scaffold_RPE1.sh
@@ -3,16 +3,12 @@
 #CHR_list=('chr1' 'chr2' 'chr3' 'chr4' 'chr5' 'chr6' 'chr7' 'chr8' 'chr9' 'chr10' 'chr11' 'chr12' 'chr13' 'chr14' 'chr15' 'chr16' 'chr17' 'chr18' 'chr19' 'chr20' 'chr21' 'chr22' 'chrX')
 CHR_list=('chr22' 'chr21' 'chr20' 'chr19' 'chr18' 'chr17' 'chr16' 'chr15' 'chr14' 'chr13' 'chr12' 'chr11' 'chr10' 'chr9' 'chr8' 'chr7' 'chr6' 'chr5' 'chr4' 'chr3' 'chr2' 'chr1' 'chrX')
 
-#input_graph_file="./output/graph_variant_jun10_BL1954_tenx_"
-#input_graph_file="./output/graph_variant_oct29_RPE1_23_hic_"
-#input_graph_file="./output/graph_variant_oct29_RPE1_22_hic_"
 input_graph_file="./output/graph_variant_nov7_RPE1_24_hic_"
 input_hap_file="./output/hap_solution_nov7_RPE1_"
 string_id="nov10_RPE1_24"
 nohup_file="nohup_scaffold_"$string_id"_"
 
 #block_cutoff=-1800
-#block_cutoff=-2000
 block_cutoff=-700
 
 for chr_choice in "${CHR_list[@]}"
@@ -25,6 +21,4 @@ do
         echo $string_id
         nohup ./linker scaffold -c $chr_choice -i $loop_hap_file -g $loop_graph_file -n $string_id -e $block_cutoff > $loop_nohup_file &
 done
-
-
 
